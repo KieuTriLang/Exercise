26070ccc0415579b76b4c83e33cb60b94799f1b4
KieuTriLang <75623308+KieuTriLang@users.noreply.github.com>
2021-03-11 16:18:10 +0700

    push 10 Mar 2021

--- Php/Exercise Php/10 Mar 2021/Exercise1/index.php
+++ Php/Exercise Php/10 Mar 2021/Exercise1/index.php
@@ -13,12 +13,36 @@
 </head>
 
 <body>
+    <?php
+    $username = $password = $notice = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_POST["username"] == "admin" && $_POST["password"] == "admin") {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+        } else {
+            $notice = "Thông tin đăng nhập không chính xác.Hãy kiểm tra lại!";
+        }
+    }
+
+    ?>
     <div class="container-fluid">
-        <form action="" class="form-group" method="POST">
-            <input type="text" name="userName" id="userName" placeholder="Username" required> <br>
-            <input type="text" name="passWord" id="passWord" placeholder="Password" required> <br>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="w-50 mx-auto text-center rounded-top" method="POST">
+            <div class=" bg-success">Sign in</div>
+            <input class="w-75 mt-3" type="text" name="username" id="userName" placeholder="Username" required> <br>
+            <input class="w-75 mt-3" type="text" name="password" id="passWord" placeholder="Password" required> <br>
+            <input class="w-75 mt-3 bg-success" type="submit" name="login" id="loginBtn" value="Login">
         </form>
+
     </div>
+    <div class="text-center">
+        <?php
+        echo $notice;
+        echo $username . "<br>";
+        echo $password;
+        ?>
+    </div>
+
+
 </body>
 
 </html>
